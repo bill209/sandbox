650ef0b0a7b9802c3fe17e8f9b4c240e10ed6b4b
bill r <jy@ozzis.us>
2017-04-14 11:15:26 -0400

    firebase simple

---
+++ firebase/simple/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { ObservableComponent } from './observable.component';
+
+// Must export the config
+export const firebaseConfig = {
+	apiKey: 'AIzaSyDPXeRTZ6zZTcV-rCEht2kwivsQXynKx2o',
+	apiKeyzzz: 'AIzaSyAM9fXabAF1eXVL59n9g2n6aMxt4M4-frA',
+	authDomain: '<your-project-authdomain>',
+	databaseURL: 'https://boiling-fire-3340.firebaseio.com',
+	storageBucket: 'gs://boiling-fire-3340.appspot.com/',
+	messagingSenderId: '<your-messaging-sender-id>'
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  declarations: [ AppComponent, ObservableComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
---
+++ firebase/simple/src/app/observable.component.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+
+@Component({
+  selector: 'app-observable',
+  template: `
+	<p>FirebaseObjectObservable</p>
+  <p>{{ (railroads | async |json) }}</p>
+  
+	<ul>
+		<li *ngFor='let key of arrayOfKeys'>
+				<h3>{{key}}:::{{(railroads | async)}}</h3>
+				<p>{{railroads[key].name}}</p>
+		</li>
+	</ul>
+
+  `,
+})
+export class ObservableComponent {
+  railroads: FirebaseObjectObservable<any>;
+	public arrayOfKeys;
+  constructor(af: AngularFire) {
+    this.railroads = af.database.object('/trains');
+		this.arrayOfKeys = Object.keys(this.railroads);
+  }
+}
