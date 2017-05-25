7a968d7d4dc537ebda24d98f2eda16f0642e3e1a
wirowlan <wirowlan@cisco.com>
2017-05-25 15:39:37 -0400

    add angular version component

--- ang_plnkr/ang_hi_world_basic/app-main.ts
+++ ang_plnkr/ang_hi_world_basic/app-main.ts
@@ -1,20 +1,21 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AngVersionComponent } from './angVersion.component.ts';
 
 @Component({
     selector: 'app-root',
-    template: '<p>{{msg}}</p>'
+    template: '<p>{{msg}}</p><ang-version></ang-version>'
 })
 
 class AppComponent {
-    msg = 'hi world';
+    msg = 'hi world'
 }
 
 
 @NgModule({
     imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, AngVersionComponent ],
     bootstrap: [ AppComponent ]
 })
 
