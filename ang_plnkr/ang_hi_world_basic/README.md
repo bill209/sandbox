4236482e4c13ebb06a6dc06d67ba8dd3eb9a654f
wirowlan <wirowlan@cisco.com>
2017-05-25 16:14:57 -0400

    refactoring ang_hi_world_basic into fewer files

--- ang_plnkr/ang_hi_world_basic/README.md
+++ ang_plnkr/ang_hi_world_basic/README.md
@@ -3,5 +3,8 @@
 
 This is a skeleton angular 4x app that prints out _hi world_
 as well as calling a second component that simply prints the
-angular version that is in use. Note: the NgModule and AppComponent
-code are both located in app.main.ts
+angular version that is in use. Note: the NgModule and Components
+are all located in app-main.ts.
+
+if you prefer them in separate files, please see
+[other Ang 4.x template](http://plnkr.co/edit/zm6j2sJEfeyKTtjCvix1?p=preview).
--- ang_plnkr/ang_hi_world_basic/app-main.ts
+++ ang_plnkr/ang_hi_world_basic/app-main.ts
@@ -1,8 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AngVersionComponent } from './angVersion.component.ts';
+import { VERSION } from '@angular/core';
 
+// appComponent --------------------------- //
 @Component({
     selector: 'app-root',
     template: '<p>{{msg}}</p><ang-version></ang-version>'
@@ -12,15 +13,27 @@ class AppComponent {
     msg = 'hi world'
 }
 
+// angVersionComponent --------------------------- //
+@Component({
+    selector: 'ang-version',
+    template: '<small>angular version: {{version}}</small>'
+})
 
+export class AngVersionComponent {
+    version:string = '0.0.0';
+
+    constructor() {
+        this.version = VERSION.full;
+//    console.log(VERSION.full); // print 2.3.0-rc.0
+    }
+}
+
+// NgModule --------------------------- //
 @NgModule({
     imports: [ BrowserModule ],
     declarations: [ AppComponent, AngVersionComponent ],
     bootstrap: [ AppComponent ]
 })
 
-class AppModule {
-
-}
-
+class AppModule { }
 platformBrowserDynamic().bootstrapModule(AppModule);
