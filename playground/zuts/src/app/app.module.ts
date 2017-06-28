e09851705dc7bd4e05dd29ba668a344fccf3690c
wirowlan <wirowlan@cisco.com>
2017-06-28 11:27:06 -0400

    adding zuts

---
+++ playground/zuts/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FooComponent } from './foo.component';
+import { BarDirective } from './bar.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooComponent,
+    BarDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
