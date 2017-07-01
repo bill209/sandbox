65e7db1eba900e33c7d0733e4291b581647df78e
bill r <jy@ozzis.us>
2017-07-01 18:45:55 -0400

    adding es6 promise example

---
+++ es2015/misc/promises.js
@@ -0,0 +1,31 @@
+// Async call
+
+let asyncCall = (x) => {
+	return new Promise((resolve, reject) => {
+		if (x !== 3) {
+			resolve('we like the number ' + x);
+		} else {
+			reject('we do not like the number 3');
+		}
+	});
+};
+
+let nbrs = [1, 3, 5];
+
+// one way to handle errors
+for (let nbr of nbrs) {
+	asyncCall(nbr).then(
+		(val) => console.log("success: " + val),
+		(err) => console.log("rejected: " + err)
+	);
+}
+
+// a second way of handling errors
+for (let nbr of nbrs) {
+	asyncCall(nbr).then(
+		(val) => console.log("success: " + val)
+	)
+		.catch(
+			(err) => console.log("rejected: " + err)
+		);
+}
