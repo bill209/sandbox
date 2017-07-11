422c2fbe86c9acc4747351e0f5887c4697211392
bill r <jy@ozzis.us>
2017-07-11 15:28:41 -0400

    udemy 3-18

--- react_learning/public/app.jsx
+++ react_learning/public/app.jsx
@@ -8,10 +8,10 @@ var GreeterMessage = React.createClass({
 
 		return (
 			<div>
-				<h1>default: {name0}</h1>
-				<h1>prop: {name1}</h1>
-				<h1>prop(obj): {name2}</h1>
-				<h1>input: {name3}</h1>
+				<p>default: {name0}</p>
+				<p>prop: {name1}</p>
+				<p>prop(obj): {name2}</p>
+				<p>input: {name3}</p>
 				<p>{msg}</p>
 			</div>
 		)
@@ -25,18 +25,21 @@ var GreeterMessage = React.createClass({
 var GreeterForm = React.createClass({
 	render: function(){
 		return (
-				<form onSubmit={this.onButtonClick}>
-					<input type="text" ref="name3"/>
+				<form onSubmit={this.onFormSubmit}>
+					<p>name: <input type="text" ref="name3"/></p>
+					<p>message: <textarea ref="text3"></textarea></p>
 					<button>Set Name</button>
 				</form>
 		)
 	},
-	onButtonClick: function(e){
+	onFormSubmit: function(e){
 		e.preventDefault();
 		var name3 = this.refs.name3.value;
+		var text3 = this.refs.text3.value;
 		if(name3.length > 0){
 			this.refs.name3.value = '';
-			this.props.onNewName(name3);
+			this.refs.text3.value = '';
+			this.props.onNewName(name3, text3);
 		}
 	}
 })
@@ -45,17 +48,19 @@ var Greeter = React.createClass({
 	render: function(){
 		var name2 = { fname : 'bill', lname : 'bob' };
 		var name3 = this.state.name3;
+		var text3 = this.state.text3;
 
 		return(
 			<div>
-				<GreeterMessage name1='bilbob' name2={name2.fname} name3={name3} msg="whazzup"/>
+				<GreeterMessage name1='bilbob' name2={name2.fname} name3={name3} msg={text3}/>
 				<GreeterForm onNewName={this.handleNewName}/>
 			</div>
 		);
 	},
-	handleNewName: function(name3){
+	handleNewName: function(name3, text3){
 		this.setState({
-			name3: name3
+			name3: name3,
+			text3: text3
 		})
 	},
 	getInitialState: function(){
